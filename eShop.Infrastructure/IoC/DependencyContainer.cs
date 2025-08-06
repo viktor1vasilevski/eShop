e4413ef6dcc83d2c4c29b866935eef51f9eff5e5
Viktor <vasilevskiviktor@yahoo.com>
2025-08-07 01:17:52 +0200

    add IoC container

---
+++ eShop.Infrastructure/IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using eShop.Application.Interfaces;
+using eShop.Application.Services;
+using eShop.Domain.Interfaces;
+using eShop.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eShop.Infrastructure.IoC;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection AddIoCService(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped<IAuthService, AdminAuthService>();
+
+
+        return services;
+    }
+}
