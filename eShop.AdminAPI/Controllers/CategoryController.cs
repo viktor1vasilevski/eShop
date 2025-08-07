2ce1c0560664529b47831e3e61433e3e2b05f537
Viktor <vasilevskiviktor@yahoo.com>
2025-08-07 12:39:58 +0200

    add category controller and request

---
+++ eShop.AdminAPI/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.AdminAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+    public class CategoryController : BaseController
+    {
+    }
+}
---
+++ eShop.Application/Requests/Category/CategoryRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace eShop.Application.Requests.Category;
+
+public class CategoryRequest : BaseRequest
+{
+    public string? Name { get; set; }
+}
