275f73ab16b972d326fa7ef44522936a4eeb9e45
Viktor <vasilevskiviktor@yahoo.com>
2025-08-11 21:38:20 +0200

    add order controller

---
+++ eShop.PublicAPI/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using eShop.Application.Interfaces;
+using eShop.Application.Requests.Order;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.PublicAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController(IOrderService _orderService) : BaseController
+{
+
+    [HttpGet]
+    public IActionResult Get([FromQuery] OrderRequest request)
+    {
+        var response = _orderService.GetOrders(request);
+        return HandleResponse(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PlaceOrderAsync([FromBody] PlaceOrderRequest request)
+    {
+        var response = await _orderService.PlaceOrderAsync(request);
+        return HandleResponse(response);
+    }
+}
