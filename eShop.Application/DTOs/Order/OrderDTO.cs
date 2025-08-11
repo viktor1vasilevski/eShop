2f4786665a1ba69a33c6f5de2fd08851310decdf
Viktor <vasilevskiviktor@yahoo.com>
2025-08-11 21:36:51 +0200

    add order dtos

---
+++ eShop.Application/DTOs/Order/OrderDTO.cs
@@ -0,0 +1,11 @@
+﻿using eShop.Application.DTOs.OrderItem;
+
+namespace eShop.Application.DTOs.Order;
+
+public class OrderDTO
+{
+    public List<OrderItemDTO>? Items { get; set; }
+    public decimal TotalAmount { get; set; }
+    public DateTime OrderCreatedOn { get; set; }
+
+}
