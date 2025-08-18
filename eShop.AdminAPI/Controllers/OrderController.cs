7097065199ab7035742a25f5bef9650f253faafb
Viktor <vasilevskiviktor@yahoo.com>
2025-08-19 00:13:17 +0200

    add order controller for admin

---
+++ eShop.AdminAPI/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using eShop.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.AdminAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController(IOrderService _orderService) : BaseController
+{
+}
