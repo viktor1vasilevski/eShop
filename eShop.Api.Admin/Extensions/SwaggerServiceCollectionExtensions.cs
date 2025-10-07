c2729776e0021423da3fe96f4263bf28993d8c3a
Viktor <vasilevskiviktor@yahoo.com>
2025-10-07 12:20:30 +0200

    add some nuget packages

---
+++ eShop.Api.Admin/Extensions/SwaggerServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace eShop.Api.Admin.Extensions;
+
+public static class SwaggerServiceCollectionExtensions
+{
+    public static IServiceCollection AddSwaggerWithAuth(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "eShop.Api.Admin",
+                Version = "v1"
+            });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                Description = "Enter 'Bearer' [space] and then your valid JWT token.\r\n\r\nExample: \"Bearer eyJhbGciOi...\""
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+        });
+
+        return services;
+    }
+}
--- eShop.Api.Admin/Program.cs
+++ eShop.Api.Admin/Program.cs
@@ -1,21 +1,36 @@
+using eShop.Api.Admin.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerWithAuth();
+
+builder.Services.AddCors(policy => policy.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "eShop.Admin API");
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseCors("MyPolicy");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
