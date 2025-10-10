0b276fdb36efbc83647204aa17b7c74e1cf2e498
Viktor <vasilevskiviktor@yahoo.com>
2025-10-10 15:37:17 +0200

    add global exception handler for customer

---
+++ eShop.Api.Customer/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,26 @@
+﻿using eShop.Application.Enums;
+using eShop.Application.Responses.Shared.Base;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace eShop.Api.Customer.Middlewares;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> _logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
+
+        var response = new ApiResponse<object>
+        {
+            Data = null,
+            Message = "An unexpected error occurred.",
+            Status = ResponseStatus.Error,
+        };
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        httpContext.Response.ContentType = "application/json";
+        await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
+
+        return true;
+    }
+}
